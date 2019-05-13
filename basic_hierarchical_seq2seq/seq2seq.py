0c8c4617b87aec17cf79878956a72d959c8e3d01
hongyunyan <649330952@qq.com>
2019-05-13 17:06:08 +0800

    修改

--- basic_hierarchical_seq2seq/seq2seq.py
+++ basic_hierarchical_seq2seq/seq2seq.py
@@ -126,7 +126,7 @@ class Seq2SeqSumm(nn.Module):
         states = init_dec_states
         for i in range(max_len):
             tok = torch.tensor(special_word_num + i).expand(states[1].size()[0], 1).cuda()
-            states, converage ,_= self._decoder.decode_step(tok, states, attention, converage)
+            states, converage = self._decoder.decode_step(tok, states, attention, converage)
             (h,c), dec_out = states
 
             h_output.append(h[0])
